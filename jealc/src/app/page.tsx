d07026f05a92f932a1e4653671cc302d0b72be2e
jean luc <jeanluc.xii.iv@gmail.com>
2025-07-12 23:27:43 +0300

    simple footer

--- jealc/src/app/page.tsx
+++ jealc/src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero';
 import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
 import ParallaxProvider from './components/ParallaxProvider';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <AboutSection />
         <ProjectsSection />
         <ContactSection />
+        <Footer />
       </main>
     </ParallaxProvider>
   );
