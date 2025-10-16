eedffb76b223e8fa85c442120baff26bd423dbc2
pacc-jean <jeanluc.xii.iv@gmail.com>
2025-10-16 20:14:54 +0300

    disable gianna button

--- jealc/src/app/page.tsx
+++ jealc/src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from './components/Hero';
 import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
-import GianaChatButton from './components/GianaChatButton';
+// import GianaChatButton from './components/GianaChatButton';
 import Footer from './components/Footer';
 import ParallaxProvider from './components/ParallaxProvider';
 
@@ -14,7 +14,7 @@ export default function Home() {
         <AboutSection />
         <ProjectsSection />
         <ContactSection />
-        <GianaChatButton />
+        {/*<GianaChatButton />*/}
         <Footer />
       </main>
     </ParallaxProvider>
